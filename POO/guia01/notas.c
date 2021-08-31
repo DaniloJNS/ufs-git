7a1caac1db8e0aed5cd4c7e35e0e5af658907271
DaniloDEV <dan.silva.13.zx@gmail.com>
2021-08-31 17:03:00 -0300

    Solution

---
+++ POO/guia01/notas.c
@@ -0,0 +1,29 @@
+// Nota final: (U1*p1 + U2*p2 + U3*p3) / (p1+p2+p3)
+// U1:Guias de Laboratório da Unidade 1 (20%)
+// U2:Guias de Laboratório da Unidade 2 (20%)
+// PF: Apresentação Projeto Final (60%)
+
+#include <stdio.h>
+
+/*! \struct notas_t
+ *  \brief Brief struct description
+ *
+ *  Detailed description
+ */
+struct aluno {
+    char nome[20];
+    int notaU1;
+    int notaU2;
+    int notaP;
+    /*!< Detailed description */
+} /* optional variable list */;
+
+void novoAluno(){
+
+}
+
+int main(int argc, char *argv[])
+{
+        
+    return 0;
+}
